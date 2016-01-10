69edc85fa3306b3cf3e3094463f742ad0fe300a8
SerhiySharga <admin@Mac-Admin.local>
2016-01-10 20:16:40 +0200

    Initial Commit

---
+++ Drows/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drows
+//
+//  Created by Admin on 10.01.16.
+//  Copyright (c) 2016 Serhiy_Sharga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
